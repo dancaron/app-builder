7594324f33dc2a82eb6310a3208496786ddf1272
Dan Caron <dancaron@gmail.com>
2023-12-08 13:40:19 -0700

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# Health Universe App Builder
+Health Universe App Builder is a sophisticated tool designed to assist data scientists in creating apps for Health Universe. This application utilizes the ChatGPT API with a specialized prompt format to leverage Large Language Models (LLMs) in providing scientifically and medically accurate Streamlit and FastAPI code. The Health Universe App Builder aims to enhance the app development and deployment process, offering a streamlined and efficient way to generate application code and documentation.
+
+## Setting up the environment
+To set up the environment, first create the environment: `conda create --name app_builder`.
+Then, activate the environment with `conda activate app_builder`.
+Then, to install the necessary packages, run `pip install requirements.txt`.
+This app requires an OpenAI API key to run. To connect the app to your OpenAI Key, run `export OPENAI_KEY = <your_key>` in your terminal.
+
+## Running the app
+After setting up the environment, launch the application with streamlit run main.py. The app will prompt you to provide an app name. The chatbot generate code and documentation in a specific format ready to be deployed on Health Universe. To generate the app use the command "build the app!"
---
+++ main.py
@@ -0,0 +1,59 @@
+import streamlit as st
+import os 
+import openai
+import pandas as pd
+from streamlit_chat import message
+
+st.set_page_config(page_title="Health Universe App Builder", page_icon=":microscope:", layout="wide")
+
+st.title('Health Universe App Builder 🩺')
+st.write('This application assists with the generation of apps that can be deployed to Health Universe.')
+
+openai.api_key = os.environ.get('OPENAI-KEY')
+
+CONTENT = open('resources/system_prompt.txt', 'r').read()
+
+def get_response():
+    message_placeholder = st.empty()
+    full_response = ""
+    messages = [{"role": "system", "content": CONTENT}] + [{"role": m["role"], "content": m["content"]} for m in st.session_state.messages]
+    if "app" in st.session_state:
+        for response in openai.ChatCompletion.create(
+            model=st.session_state["openai_model"],
+            messages= messages,
+            stream=True,
+        ):
+            full_response += response.choices[0].delta.get("content", "")
+            message_placeholder.markdown(full_response + "▌")
+    else:
+        st.session_state["app"] = messages[-1]['content']
+        full_response = f'Thank you! You replied {st.session_state["app"]}. Ok, let\'s build it!'
+    message_placeholder.markdown(full_response)
+    return full_response
+
+def main():
+    if "openai_model" not in st.session_state:
+        st.session_state["openai_model"] = "gpt-4-1106-preview"
+
+    # load previous messages, or empty list if there are no previous messages
+    if "messages" not in st.session_state:
+        st.session_state.messages = [{"role": "assistant", "content":"Hi! I'm your App Building agent. What kind of app would you like to create? Please be as specific as possible. You may include the contents of a GitHub readme or scientific paper."}]
+
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    # chatting part
+    if prompt := st.chat_input("How can I help you?"):
+        # user input
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.markdown(prompt)
+
+        # response
+        with st.chat_message("assistant"):
+            full_response = get_response()
+        st.session_state.messages.append({"role": "assistant", "content": full_response})
+
+if __name__ == "__main__":
+    main()
